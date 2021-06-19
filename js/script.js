1dbf5f196b4872ed8778eb230d6794d07ac8aadb
Alexsander <alexsandrsulimenko@gmail.com>
2021-06-19 15:37:46 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,105 @@
+// // "use strict";
+// // console.log('Я учусь JS');
+// // // 1.Ошибака! все открытое должно быть закрытым!
+// // // 2.Ошибка! соблюдение регистра!
+// // let boss = 'рефлежератор';
+// // console.log(boss);
+// // boss = 'холодильник';
+// // console.log(boss);
+// // let myAge = 35;
+// // myAge = 18;
+// // console.log(myAge);
+// // let myNewAge = 33;
+// // myAge = myNewAge;
+// // console.log(myAge);
+
+// // const userProfile = {
+// //    name: 'Александр',
+// //    age: 35,
+// //    massage: 'Живи, а работай в свободное время!'
+// // }
+// // console.log(userProfile);
+// // userProfile.age = 18;
+// // console.log(userProfile);
+
+// // let user;
+// // let userName = 'Петя';
+// // user = userName;
+// // console.log(user);
+
+
+// let willYouMarryMe = true;
+// if (willYouMarryMe) {
+//    console.log(':)');
+// } else {
+//    console.log(':(');
+// }
+
+// let trueOrFalse = 58 < 18;
+// if (trueOrFalse) {
+//    console.log('Верно');
+// } else {
+//    console.log('Не верно');
+// }
+
+// let getInfinity = 49 / 0;
+// console.log(getInfinity);
+// console.log(typeof getInfinity);
+
+// let userAge1 = 50;
+// console.log(userAge1);
+// console.log(typeof userAge1);
+// userAge1 = String(userAge1);
+// console.log(userAge1);
+// console.log(typeof userAge1);
+
+// let userTrue = true;
+// console.log(userTrue);
+// console.log(typeof userTrue);
+// userTrue = String(userTrue);
+// console.log(userTrue);
+// console.log(typeof userTrue);
+
+// let userAge = 30;
+// let userInfo = `Работаю ${userAge}`;
+// console.log(userInfo);
+
+let x;
+x = 5 + 9;
+console.log('результат сложения', x);
+
+x = 40 - 27;
+console.log(`Результат вычетания ${x}`);
+
+x = 34 % 5;
+console.log(`Результат взятия остатка от деления ${x}`);
+console.log(x);
+let resultOne = "каждый охотник" + " " + "желает знать" + " " + "где сидит" + " фазан";
+consconsole.log(resultOne);
+
+let userCounter = 0;
+let newUsers = 2 * ++userCounter;
+console.log(newUsers);
+
+let a = 1;
+let b = 'sanya';
+let c = 3;
+
+const xyi = [a, b, c, 111111, 5, 'sdsdsd']
+
+
+const obj = {
+   perviy: a,
+   name: b,
+   date: 2372637,
+   arr: xyi
+}
+
+console.log('!!!!!!!!!!!!!!!', obj)
+
+[[1, 2, 3], [1, 2, 3], [5, 8, 4]]
+
+
+const userInfo = {
+   name: Petia
+}
